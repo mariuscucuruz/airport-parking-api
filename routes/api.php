784aed37876cd110a1a54658bd35f306fb3b1892
Marius Cucuruz <marius.cucuruz@marmalade.co.uk>
2022-12-08 11:42:09 +0000

    cleanup routes definition

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', function () {
+    return App::version();
+});
 
 // maximum of 10 requests per minute seems reasonable
 Route::prefix('check-availability')->middleware(['api', 'throttle:10'])->group(function () {
@@ -24,10 +17,7 @@ Route::prefix('check-availability')->middleware(['api', 'throttle:10'])->group(f
     Route::delete('/{booking}', [AvailabilityController::class, 'delete']);
 });
 
-Route::get('/', function () {
-    return App::version();
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// handling authentication is out of the scope of this test
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
