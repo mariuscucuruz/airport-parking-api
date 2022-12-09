23dba23a52a20abee8a541a5b8c95fec92d26462
Marius Cucuruz <marius.cucuruz@marmalade.co.uk>
2022-12-09 12:49:32 +0000

    wip

--- app/Services/AvailabilityService.php
+++ app/Services/AvailabilityService.php
@@ -3,33 +3,44 @@
 namespace App\Services;
 
 use App\Models\Booking;
-use http\Exception\InvalidArgumentException;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AvailabilityService
 {
+    public const MAX_DAILY_SPACES = 10;
+
     public function isAvailable(mixed $payload): bool
     {
-        return (bool) $this->findBooking($payload);
+        return (self::MAX_DAILY_SPACES >= $this->findBookings($payload)->count());
     }
 
     public function reserveDates(array $payload): Booking
     {
+        if (!$this->isAvailable($payload)) {
+            throw new Exception('Slot not available.');
+        }
+
         return Booking::create($payload);
     }
 
     public function changeReservation(Booking $booking, array $payload): Booking
     {
-        if ($this->findBooking($booking->toArray())) {
+        if ($this->findBookings($booking->toArray())) {
             $booking->update($payload);
 
             return $booking->save();
         }
 
-        throw new InvalidArgumentException();
+        throw new ModelNotFoundException();
     }
 
-    private function findBooking(array $criteria): ?Booking
+    /**
+     * @param array $criteria
+     * @return \App\Models\Booking[]|null
+     */
+    private function findBookings(array $criteria): ?Booking
     {
-        return Booking::where($criteria)->first();
+        return Booking::where($criteria)->get();
     }
 }
