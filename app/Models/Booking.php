c1093d0cc8d744810ddf8348f2e566131a023e77
Marius Cucuruz <marius.cucuruz@marmalade.co.uk>
2022-12-09 11:39:37 +0000

    implemented booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Booking extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'bookings';
+    public $timestamps = true;
+    protected $dateFormat = 'Y-m-d';
+    protected $guarded = [];
+
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query
+            ->where('dateStart', '<=', now()->format($this->dateFormat))
+            ->where('dateEnd', '>', now()->format($this->dateFormat));
+    }
+
+//    public function user(): BelongsTo
+//    {
+//        return $this->belongsTo(User::class);
+//    }
 }
---
+++ database/migrations/2022_12_09_100959_create_bookings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Booking;
+
+return new class extends Migration
+{
+    static function dbTableName(): string
+    {
+        return (new Booking())->getTable();
+    }
+
+    public function up()
+    {
+        Schema::create(self::dbTableName(), function (Blueprint $table) {
+            $table->id();
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists(self::dbTableName());
+    }
+};
